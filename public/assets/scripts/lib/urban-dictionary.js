160586caa02bc905e44ff2685f228a7740cacd3c
Jessada Trirongkit <jessada.trirongkit@gmail.com>
2018-07-14 17:30:50 +0700

    Added alert for over fetch

--- public/assets/scripts/lib/urban-dictionary.js
+++ public/assets/scripts/lib/urban-dictionary.js
@@ -1,15 +1,23 @@
 function createUrbanDictionary () {
   let randomCache = []
+  let isFetching = false
 
   return {
     random () {
+      if (isFetching) {
+        alert('ใจเย็นๆ: Calm down')
+        return Promise.reject('ใจเย็นๆ: Calm down')
+      }
+      isFetching = true
       if (randomCache[0]) {
+        isFetching = false
         return Promise.resolve(randomCache.shift())
       }
 
       return fetch('http://api.urbandictionary.com/v0/random')
         .then(response => response.json())
         .then((data) => {
+          isFetching = false
           randomCache = data.list
           return randomCache.shift()
         })
